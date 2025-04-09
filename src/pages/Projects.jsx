19a48a22d7471b78eb20f488904a26ebf8b2165a
chandu-uias <chandrashekaruias@gmail.com>
2025-04-09 12:27:36 +0530

    novacapture project added

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -5,6 +5,30 @@ const Projects = () => {
     <div className="max-w-4xl mx-auto">
       <h1 className="text-3xl font-bold text-gray-800 mb-8">Projects</h1>
       <div className="grid md:grid-cols-2 gap-8">
+      <div className="bg-white rounded-lg shadow-md overflow-hidden">
+          <img
+            src="https://res.cloudinary.com/dkf7alzki/image/upload/v1744181778/Screenshot_71_j7he99.png"
+            alt="Project 1"
+            className="w-full h-48 object-cover"
+          />
+          <div className="p-6">
+            <h2 className="text-xl font-semibold text-gray-800 mb-2">NovaCapture – Online Photography Booking Platform</h2>
+            <p className="text-gray-600 mb-4">
+            NovaCapture is an all-in-one platform designed to bridge the gap between clients and photographers, 
+            making the process of discovering, booking, and managing photography services seamless and efficient.
+            </p>
+            <div className="flex space-x-4">
+              <a href="https://github.com/chandu-uias/Nova.git" className="text-blue-600 hover:text-blue-800 flex items-center">
+                <Github size={18} className="mr-1" />
+                Code
+              </a>
+              <a href="https://nova-j9db.vercel.app/" className="text-blue-600 hover:text-blue-800 flex items-center">
+                <ExternalLink size={18} className="mr-1" />
+                Live Demo
+              </a>
+            </div>
+          </div>
+        </div>
         <div className="bg-white rounded-lg shadow-md overflow-hidden">
           <img
             src="https://res.cloudinary.com/dkf7alzki/image/upload/v1743916645/Screenshot_55_bjpohg.png"
@@ -29,6 +53,7 @@ const Projects = () => {
             </div>
           </div>
         </div>
+      
 
         <div className="bg-white rounded-lg shadow-md overflow-hidden">
           <img
@@ -40,7 +65,7 @@ const Projects = () => {
             <h2 className="text-xl font-semibold text-gray-800 mb-2">StudyNotion – Build, Learn, and Grow with a Modern Ed-Tech Platform</h2>
             <p className="text-gray-600 mb-4">
             StudyNotion is a fully functional ed-tech platform that enables users to create, consume, and rate educational content.
-The platform is built using the MERN stack, which includes ReactJS, NodeJS, MongoDB, and ExpressJS.
+            The platform is built using the MERN stack, which includes ReactJS, NodeJS, MongoDB, and ExpressJS.
 
             </p>
             <div className="flex space-x-4">
